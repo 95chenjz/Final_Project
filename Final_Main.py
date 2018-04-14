a44519ac53821ec8364d77f43ec8890aeb50f95c
YichongGuo <yichong2@illinois.edu>
2018-04-14 16:42:02 -0500

    heiheihei

--- Final_Main.py
+++ Final_Main.py
@@ -1,9 +1,10 @@
 import random
+import ggplot as gp
 common=49
 rare=36
 epic=27
-lengendary=23
-frozen=[common,rare,epic,lengendary]
+legendary=23
+frozen=[common,rare,epic,legendary]
 pc=0.7016
 pr=0.2174
 pe=0.0395
@@ -16,19 +17,22 @@ weights=[pc,pr,pe,pl,pgc,pgr,pge,pgl]
 
 
 def packopen(expansion,weights):
-    level=['common','rare','epic','lengendary','goldencommon','goldenrare','goldenepic','goldenlengendary']
+    level=['common','rare','epic','legendary','goldencommon','goldenrare','goldenepic','goldenlegendary']
     cardsget=[]
-    for i in range(5):
-        cardlevel=random.choices(level,weights)
-        if cardlevel[0]=='common' or cardlevel[0]=='goldencommon':
-            cardsget.append(cardlevel[0]+','+str(random.randint(1,expansion[0])))
-        elif cardlevel[0]=='rare' or cardlevel[0]=='goldenrare':
-            cardsget.append(cardlevel[0] + ','+str(random.randint(1, expansion[1])))
-        elif cardlevel[0]=='epic' or cardlevel[0]=='goldenepic':
-            cardsget.append(cardlevel[0] +','+ str(random.randint(1, expansion[2])))
-        elif cardlevel[0]=='lengendary' or cardlevel[0]=='goldenlengendary':
-            cardsget.append(cardlevel[0] +','+ str(random.randint(1, expansion[3])))
+    cardlevel = random.choices(level, weights, k=5)
+
+    for level in cardlevel:
+        if level == 'common' or level == 'goldencommon':
+            cardsget.append((level, random.randint(1,expansion[0])))
+        elif level == 'rare' or level == 'goldenrare':
+            cardsget.append((level, random.randint(1, expansion[1])))
+        elif level == 'epic' or level == 'goldenepic':
+            cardsget.append((level, random.randint(1, expansion[2])))
+        elif level == 'legendary' or level == 'goldenlegendary':
+            cardsget.append((level, random.randint(1, expansion[3])))
+    #print(cardsget)
     return cardsget
+
 dustgetc=5
 dustgetr=20
 dustgete=100
@@ -41,56 +45,67 @@ dustusec=40
 dustuser=100
 dustusee=400
 dustusel=1600
-owned={}
-dustneeded=2*common*dustusec+2*rare*dustuser+2*epic*dustusee+lengendary*dustusel
-dusthave=0
-packopened=0
-while dustneeded>dusthave:
-    cardsget=packopen(frozen,weights)
-    packopened+=1
-    for card in cardsget:
-        if card.split(',')[0]=='goldencommon':
-            dusthave+=dustgetgc
-        elif card.split(',')[0]=='goldenrare':
-            dusthave += dustgetgr
-        elif card.split(',')[0]=='goldenepic':
-            dusthave += dustgetge
-        elif card.split(',')[0]=='goldenlengendary':
-            dusthave += dustgetgl
-        elif card.split(',')[0]=='common':
-            if card not in owned.keys():
-                owned[card]=1
-                dustneeded-=dustusec
-            elif owned[card]==1:
-                owned[card]=2
-                dustneeded -= dustusec
-            else:
-                dusthave+=dustgetc
-        elif card.split(',')[0]=='rare':
-            if card not in owned.keys():
-                owned[card]=1
-                dustneeded-=dustuser
-            elif owned[card]==1:
-                owned[card]=2
-                dustneeded -= dustuser
-            else:
-                dusthave+=dustgetr
-        elif card.split(',')[0]=='epic':
-            if card not in owned.keys():
-                owned[card]=1
-                dustneeded-=dustusee
-            elif owned[card]==1:
-                owned[card]=2
-                dustneeded -= dustusee
-            else:
-                dusthave+=dustgete
-        elif card.split(',')[0]=='lengendary':
-            if card not in owned.keys():
-                owned[card]=1
-                dustneeded-=dustusel
-            else:
-                dusthave+=dustgetl
+
+def get_all_cards(common:int, rare:int, epic:int, legendary:int):
+    owned = {}
+    packopened = 0
+    dustneeded = 2 * common * dustusec + 2 * rare * dustuser + 2 * epic * dustusee + legendary * dustusel
+    dusthave = 0
+
+    while dustneeded > dusthave:
+
+        cardsget = packopen(frozen,weights)
+        packopened += 1
+
+        for card in cardsget:
+            if card[0]=='goldencommon':
+                dusthave += dustgetgc
+            elif card[0]=='goldenrare':
+                dusthave += dustgetgr
+            elif card[0]=='goldenepic':
+                dusthave += dustgetge
+            elif card[0]=='goldenlegendary':
+                dusthave += dustgetgl
+            elif card[0] == 'common':
+                if card not in owned.keys():
+                    owned[card] = 1
+                    dustneeded -= dustusec
+                elif owned[card] == 1:
+                    owned[card] = 2
+                    dustneeded -= dustusec
+                else:
+                    dusthave += dustgetc
+            elif card[0] == 'rare':
+                if card not in owned.keys():
+                    owned[card] = 1
+                    dustneeded -= dustuser
+                elif owned[card] == 1:
+                    owned[card] = 2
+                    dustneeded -= dustuser
+                else:
+                    dusthave += dustgetr
+            elif card[0] == 'epic':
+                if card not in owned.keys():
+                    owned[card] = 1
+                    dustneeded -= dustusee
+                elif owned[card] == 1:
+                    owned[card] = 2
+                    dustneeded -= dustusee
+                else:
+                    dusthave += dustgete
+            elif card[0] == 'legendary':
+                if card not in owned.keys():
+                    owned[card] = 1
+                    dustneeded -= dustusel
+                else:
+                    dusthave += dustgetl
+    return packopened
+
 total=0
-for i in range(10000000):
-    total+=packopened
-print(total/10000000)
+
+for i in range(10000):
+    times = get_all_cards(common, rare, epic, legendary)
+    total += times
+    print(times)
+
+print(total/10000)
