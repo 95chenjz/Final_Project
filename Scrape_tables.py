c7ac1f4a97e2e12a92ebc769a94d248b6dadfc75
Jianzhang Chen <95chenjz@sina.com>
2018-04-15 16:36:23 -0500

    Scrape probability table from web

---
+++ Scrape_tables.py
@@ -0,0 +1,29 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+
+def get_prob_table(url):
+
+    page = requests.get(url)
+    html = page.content
+
+    soup = BeautifulSoup(html, 'lxml')
+
+    tables = soup.find_all('table',{'class':'wikitable'})
+    tb = tables[3]
+
+    df = pd.DataFrame(0,index = range(0,7),columns=['Expansion','Total','Common','Rare','Epic','Legendary',\
+                      'Golden_Total','Golden_Common','Golden_Rare','Golden_Epic',\
+                      'Golden_Legendary'])
+
+    df.Expansion = [head.contents[0] for head in tb.find_all('th')[14:21]]
+
+    for i, row in enumerate(tb.find_all('tr')[2:9]):
+        tdx = [val for val in row.find_all('td')[1:]]
+        df.iloc[i:i+1,1:] = [float(i.contents[0].strip().strip('%'))/100 for i in tdx]
+
+    return df
+
+
+url = 'https://hearthstone.gamepedia.com/Card_pack_statistics'
+prob_table = get_prob_table(url)
